e091bad2739a36ef6a1d9025b0b88243bf1be14a
thallard <thallard@student.42lyon.fr>
2020-12-24 15:16:31 +0100

    Norme on screenshot part

--- srcs/ft_bmp.c
+++ srcs/ft_bmp.c
@@ -6,7 +6,7 @@
 /*   By: thallard <thallard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/24 13:05:21 by thallard          #+#    #+#             */
-/*   Updated: 2020/12/24 15:11:49 by thallard         ###   ########lyon.fr   */
+/*   Updated: 2020/12/24 15:14:24 by thallard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,30 +44,21 @@ void	ft_init_bmp(t_all *a, t_mlx_info *i)
 
 void	ft_fill_bmp_header(t_all *a)
 {
-	ft_memcpy(a->bmp->header, "BM", 2);
-	ft_memcpy(a->bmp->header + 2, &a->bmp->file_s, 4);
-	ft_memcpy(a->bmp->header + 10, &a->bmp->header_s, 4);
-	ft_memcpy(a->bmp->header + 14, &a->bmp->bytes_s, 4);
-	ft_memcpy(a->bmp->header + 18, &a->bmp->w, 4);
-	ft_memcpy(a->bmp->header + 22, &a->bmp->height, 4);
-	ft_memcpy(a->bmp->header + 26, &a->bmp->biplanes, 4);
-	ft_memcpy(a->bmp->header + 28, &a->bmp->bytes_number, 4);
-	ft_memcpy(a->bmp->header + 34, &a->bmp->image_s, 4);
+	int		i;
+
+	ft_memmove(a->bmp->header, "BM", 2);
+	ft_memmove(a->bmp->header + 2, &a->bmp->file_s, 4);
+	ft_memmove(a->bmp->header + 10, &a->bmp->header_s, 4);
+	ft_memmove(a->bmp->header + 14, &a->bmp->bytes_s, 4);
+	ft_memmove(a->bmp->header + 18, &a->bmp->w, 4);
+	ft_memmove(a->bmp->header + 22, &a->bmp->height, 4);
+	ft_memmove(a->bmp->header + 26, &a->bmp->biplanes, 4);
+	ft_memmove(a->bmp->header + 28, &a->bmp->bytes_number, 4);
+	ft_memmove(a->bmp->header + 34, &a->bmp->image_s, 4);
 	a->bmp->fd = open("./screenshot.bmp", O_TRUNC | O_WRONLY | O_CREAT, 0777);
-	ft_write_screenshot_in_bmp(a);
-}
-
-void	ft_write_screenshot_in_bmp(t_all *a)
-{
-	int		height_to_write;
-
-	height_to_write = a->bmp->height;
+	i = a->bmp->height;
 	write(a->bmp->fd, a->bmp->header, 54);
-	while (height_to_write)
-	{
-		write(a->bmp->fd, a->infos->int_img
-		+ (height_to_write * a->bmp->w), a->bmp->w * 4);
-		height_to_write--;
-	}
+	while (i != 0)
+		write(a->bmp->fd, a->infos->int_img + (i-- * a->bmp->w), a->bmp->w * 4);
 	close(a->bmp->fd);
 }
